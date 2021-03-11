8b622da90b8f14e9d4fccc8c1ad31ebe1130f3e6
LDoan1 <dugraallancelot@gmail.com>
2021-03-11 10:12:56 +0100

    debut class perso

---
+++ scripts/personnage.py
@@ -0,0 +1,16 @@
+class personnage : 
+    def __init__(self):
+        pass
+
+    def bouger(self, deplacement ): #déplacement sous la forme de tuples (dep x, dep y)
+
+        pass
+
+    def prendre_objet(self):
+        pass
+
+    def attaquer(self):
+        pass
+
+    
+
