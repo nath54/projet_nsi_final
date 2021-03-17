844b8ab1f78830e8195bd193202f0ffca32be2ac
nathan cerisara <cerisara.nathan@gmail.com>
2021-03-17 11:40:19 +0100

    changement on websocket

--- web/js/webscocket_client.js
+++ web/js/webscocket_client.js
@@ -1,21 +1,26 @@
-// Ip auquel se connecter, a recevoir du menu principal
-// Ici pour l'instant car il n'y a pas encore de menu principal
-const IP = "localhost";
-// Port auquel se connecter, a recevoir du menu principal
-// Ici pour l'instant car il n'y a pas encore de menu principal
-const PORT = 6546;
+// // Ip auquel se connecter, a recevoir du menu principal
+// // Ici pour l'instant car il n'y a pas encore de menu principal
+// const IP = "localhost";
+// // Port auquel se connecter, a recevoir du menu principal
+// // Ici pour l'instant car il n'y a pas encore de menu principal
+// const PORT = 6546;
+
+// !!! important !!!
+// On initiera la variable ws_url avec le php avant d'insérer ce script
+// !!! important !!!
 
 var websocket = null; // On prépare la variable globale
 
 function start_websocket() {
     /**
      * Fonction qui initialise et lance le serveur websocket
-     * 
+     *
      * @author Nathan
      */
 
     // On se connecte au websocket
-    websocket = new WebSocket("ws://" + IP + ":" + PORT + "/");
+    // websocket = new WebSocket("ws://" + IP + ":" + PORT + "/");
+    websocket = new WebSocket(ws_url);
 
     // Quand il y a des erreurs
     websocket.onerror = function() {
@@ -29,12 +34,12 @@ function start_websocket() {
 }
 
 // Fonction pour envoyer des messages
-function send_mes(message) {
+function ws_send(message) {
     /**
      * Envoie un message au websocket en convertissant le message en JSON
-     * 
+     *
      * @params message : Message a envoyer, sous la forme d'un dictionnaire
-     * 
+     *
      * @author Nathan
      */
     // On convertit en json
@@ -46,9 +51,9 @@ function send_mes(message) {
 function on_message(event) {
     /**
      * Fonction qui gere tous les messages recus du serveur
-     * 
+     *
      * @params event : Evenement message géré par js
-     * 
+     *
      * @author Nathan
      */
     // On recoit les informations
