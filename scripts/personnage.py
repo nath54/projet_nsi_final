f46d9a11d07a16c3185eab0d2fec6c0effae65ee
LDoan1 <dugraallancelot@gmail.com>
2021-03-17 22:41:36 +0100

    déplacement et nouveaux tests

--- scripts/personnage.py
+++ scripts/personnage.py
@@ -4,32 +4,33 @@ class personnage :
         self.nom = str(nom)
         self.sexe = str(sexe)
         self.classe = str(classe)
-        self.position_x = 0 ## Position initiale du perso en x
-        self.position_y = 0 ## Position initiale du perso en y
+        self.position = {"x": 0, "y": 0}
         self.sprite_fixe = "penser me transmettre un sprite pour le perso quand il bouge pas"
         self.sprite_droite = "penser me transmettre un sprite pour le perso quand il va a droite"
         self.sprite_gauche = "penser me transmettre un sprite pour le perso quand il va a gauche"
         self.sprite_haut = "penser me transmettre un sprite pour le perso quand il va en haut"
         self.sprite_bas = "penser me transmettre un sprite pour le perso quand il va en bas"
         self.vie = 20
+        self.max_vie = 20
         self.niveau = 0  
         self.xp = 0
         self.stamina = 20
         self.mana = 20 
+        self.mana_max = 20
         self.armor = 0 
 
     def afficher(self):
         pass
 
-    def bouger(self, touche): #déplacement sous la forme de tuples (dep x, dep y)
+    def bouger(self, touche): #déplacement sous la forme de dict (dep x, dep y)
         if touche == "up":
-            self.position_y = self.position_y + 1
+            self.position["y"] = self.position["y"] + 1
         elif touche == "right":
-            self.position_x = self.position_x + 1
+            self.position["x"] = self.position["x"] + 1
         elif touche == "left" :
-            self.position_x = self.position_x - 1
+            self.position["x"] = self.position["x"] - 1
         elif touche == "down":
-            self.position_y = self.position_y - 1
+            self.position["y"] = self.position["y"] - 1
         
 
     def prendre_objet(self, touche):
@@ -41,14 +42,12 @@ class personnage :
         pass
 
     def interagir(self, touche):
-        pass
+        if touche == "e" or touche == "E":
+            pass
 
     def level_up(self):
         if self.xp == self.xp + 100 :
             self.niveau = self.niveau + 1
-
-    def new_position(self):
-        pass
 
     def perdre_vie(self, dgt_monstre):
         pass
@@ -58,11 +57,16 @@ class personnage :
 
 def test():
     print("début des tests")
-    p = personnage()
+    p = personnage("Lance", "mage", "homme")
 
     p.bouger("up")
-    assert self.position_x == 0
-    assert self.position_y == 1
+    assert p.position == {"x": 0, "y": 1}
+    p.bouger("left")
+    assert p.position == {"x": -1, "y": 1}
+    p.bouger("right")
+    assert p.position == {"x": 0, "y": 1}
+    p.bouger("up")
+    assert p.position == {"x": 0, "y": 2}
     
     print("fin des tests")
 
