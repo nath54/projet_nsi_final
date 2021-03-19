21f6fe334ca1e76d6e2182490e8bbe716699f505
nathan cerisara <cerisara.nathan@gmail.com>
2021-03-19 09:10:53 +0100

    aa

---
+++ rempli_bdd.sql
@@ -0,0 +1,37 @@
+
+
+INSERT INTO terrain SET
+    image_="herbe.png",
+    nom "herbe",
+    peut_marcher 1,
+	cultivable 1,
+	objet_dessus 1;
+
+INSERT INTO terrain SET
+    image_="neige.png",
+    nom "neige",
+    peut_marcher 1,
+	cultivable 0,
+	objet_dessus 1;
+
+INSERT INTO terrain SET
+    image_="sable.png",
+    nom "sable",
+    peut_marcher 1,
+	cultivable 0,
+	objet_dessus 1;
+
+INSERT INTO terrain SET
+    image_="terre.png",
+    nom "terre",
+    peut_marcher 1,
+	cultivable 1,
+	objet_dessus 1;
+
+
+INSERT INTO terrain SET
+    image_="vide.png",
+    nom "vide",
+    peut_marcher 0,
+	cultivable 0,
+	objet_dessus 0;
