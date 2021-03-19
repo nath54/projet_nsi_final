5e433e72d945d4668650d5de33df7eefde4889a9
FunkyMehdi <79305201+FunkyMehdi@users.noreply.github.com>
2021-03-19 15:57:27 +0100

    Add files via upload
    Je me doute qu'il y a des erreurs mais j'envoie ma partie

---
+++ sql_methodes.php
@@ -0,0 +1,21 @@
+<?php 
+/* définition de fonctions en lien avec SQL, appelées depuis les autres parties du programme */
+function sql_connect() {
+    // fonction permettant de se connecter à la BDD
+    // Les lignes suivantes sont à modifier selon les besoins !
+    $port = 3307;
+    $dbname = 'projetclasse';
+    $identifiant = 'projetclasse'; //cet utilisateur n'a qu'un seul droit : lecture sur base BLOC4
+    $motdepasse = 'proj#17CLASSE';
+    try
+    { 
+        $sch='mysql:host=localhost;dbname='.$dbname.";port=".$port;
+        $bdd = new PDO($sch , $identifiant, $motdepasse);
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+    return $bdd ;
+} /* se connecte à la bdd et renvoie l'objet PDO obtenu */
+?>
