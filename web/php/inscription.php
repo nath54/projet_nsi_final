2e40caa8392c09c27c5fc4ff67a2f4e7f19b633e
nathan cerisara <cerisara.nathan@gmail.com>
2021-03-22 11:28:19 +0100

    correction

--- web/php/inscription.php
+++ web/php/inscription.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../../includes/bdd.php";
-$db = load_db();
+$db = load_db("../../includes/config.json");
 // on teste si le joueur a soumis le formulaire
 	if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
 		// on cherche à savoir si les variables existent
