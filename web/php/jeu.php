cc52e8be7ef7df47a8fc4fd9e25055aece6eeb7b
nathan cerisara <cerisara.nathan@gmail.com>
2021-03-22 11:25:35 +0100

    aa

---
+++ web/php/jeu.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Jeu</title>
+        <link href="../css/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        
+    </body>
+
+</html>
