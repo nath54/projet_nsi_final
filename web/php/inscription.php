be7e6cd9b264e46ff160dc10f0f5ff83ca59a50a
FunkyMehdi <bounjemaurelie@gmail.com>
2021-03-22 11:04:51 +0100

    Update inscription.php

--- web/php/inscription.php
+++ web/php/inscription.php
@@ -2,9 +2,11 @@
 // on teste si le joueur a soumis le formulaire
 	if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
 		// on cherche à savoir si les variables existent
-		if ((isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['pseudo_confirm']) && !empty($_POST['pseudo_confirm'])))
+		if ((isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['pseudo']) && !empty($_POST['pseudo'])) && (isset($_POST['mdp_confirm']) && !empty($_POST['mdp_confirm']))){
+
+		}
 		// on teste les deux mots de passe
-		if ($_POST['pseudo'] != $_POST['pseudo_confirm']) {
+		if ($_POST['mdp'] != $_POST['mdp_confirm']) {
 			$erreur = 'Les 2 mots de passe sont différents.';
 		}
 		else {  // on se connecte à un serveur SQL
@@ -17,18 +19,18 @@
 			$data = mysql_fetch_array($req);
 
 			if ($data[0] == 0) {
-			$sql = 'INSERT INTO utilisateurs VALUES("", "'.mysql_escape_string($_POST['pseudo']).'", "'.mysql_escape_string(md5($_POST['mdp'])).'")'; // on insère un pseudo et un mot de passe dans la base ; on utilise md5 qui va hacher le mot de passe
-			$req = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error()); // 
+				$sql = 'INSERT INTO utilisateurs VALUES("", "'.mysql_escape_string($_POST['pseudo']).'", "'.mysql_escape_string(md5($_POST['mdp'])).'")'; // on insère un pseudo et un mot de passe dans la base ; on utilise md5 qui va hacher le mot de passe
+				$req = mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error()); // 
 
-			session_start();
-			$_SESSION['pseudo'] = $_POST['pseudo'];
-			header('Location: membre.php');
-			exit();
+				session_start();
+				$_SESSION['pseudo'] = $_POST['pseudo'];
+				header('Location: membre.php');
+				exit();
 			}
 			else {
-			$erreur = 'Un membre possède déjà ce pseudo.';
+				$erreur = 'Un membre possède déjà ce pseudo.';
 			}
-
+		}
 	}
 ?>
 
@@ -47,7 +49,7 @@
 
             <label for="mdp"> Mot de passe : </label> <input type="password" name="mdp" value=" <?php if (isset($_POST['mdp'])) echo htmlentities(trim($_POST['mdp'])); ?> "> <br />
 
-            <label for="mdp_confirm"> Confirmation du mot de passe : </label> <input type="password" name="mdp_confirm" value=" <?php if (isset($_POST['mdp_confirm'])) echo htmlentities(trim($_POST['pass_confirm'])); ?> "> <br /> 
+            <label for="mdp_confirm"> Confirmation du mot de passe : </label> <input type="password" name="mdp_confirm" value=" <?php if (isset($_POST['mdp_confirm'])) echo htmlentities(trim($_POST['mdp_confirm'])); ?> "> <br /> 
 
             <!-- html entities convertit tous les caractères en entités HTML -->
 
