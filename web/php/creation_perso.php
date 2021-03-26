81d2aae3c1e9ae26418431e052f2f1fd6a823806
NitoGitHub <victortw3x@gmail.com>
2021-03-26 09:11:21 +0100

    crea

--- web/php/creation_perso.php
+++ web/php/creation_perso.php
@@ -15,55 +15,52 @@
 
         <!-- Bouton continuer, qui enregistre nos choix et nous redirige vers le menu de création -->
         <!-- En sauvegardant nos choix de classe et de sexe, et nous propose des tenues adapté -->
-        <div class="continuer_creation">
-        <button class="bouton_continuer_creation">CONTINUER</button>
-        </div>
-
-
-        
-            <!-- Menu de création -->
-            <!-- Après le choix du sexe et de la classe -->
-            <div class="menu_creation_perso column">
-                <div class="row">
-                    <button><</button><a>Tete<span id="num_tete_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Couleur de peau<span id="num_cdp_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Cheveux<span id="num_cheveux_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Barbe<span id="num_barbe_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Haut<span id="num_haut_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Bas<span id="num_bas_sel">1</span>/6</a><button>></button>
-                </div>
-                <div class="row">
-                    <button><</button><a>Pied<span id="num_pied_sel">1</span>/6</a><button>></button>
-                </div>
+            <div class="continuer_creation">
+            <button class="bouton_continuer_creation">CONTINUER</button>
             </div>
 
+
             
-        <!-- Aperçu du sprite avec ses changements -->
-        <div class="sprite_creation">
-
-
-                <!-- Bouton valier, qui enregistre toutes les modifs dans la BDD -->
-                <div class="valider_creation">
+                <!-- Menu de création -->
+                <!-- Après le choix du sexe et de la classe -->
+                <div class="menu_creation_perso column">
+                    <div class="row">
+                        <button><</button><a>Tete<span id="num_tete_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Couleur de peau<span id="num_cdp_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Cheveux<span id="num_cheveux_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Barbe<span id="num_barbe_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Haut<span id="num_haut_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Bas<span id="num_bas_sel">1</span>/6</a><button>></button>
+                    </div>
+                    <div class="row">
+                        <button><</button><a>Pied<span id="num_pied_sel">1</span>/6</a><button>></button>
+                    </div>
                 </div>
 
-        <div class="">
+                
+            <!-- Aperçu du sprite avec ses changements -->
+            <div class="sprite_creation">
+
+
+                    <!-- Bouton valier, qui enregistre toutes les modifs dans la BDD -->
+                    <div class="valider_creation">
+                    </div>
+
+            <div class="">
+
+            </div>
 
         </div>
-
-    </div>
-
-
-
 
     </body>
 </html>
