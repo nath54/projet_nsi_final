886633f978d7b2a4cc17fd5e474293e247b4fea5
monkylog <79305220+monkylog@users.noreply.github.com>
2021-03-26 08:20:32 +0100

    modif accueil

--- web/php/accueil.php
+++ web/php/accueil.php
@@ -39,10 +39,9 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
 
 <html>
 	<head>
-        <meta charset="utf-8">
-        <title> Accueil </title>
-
-        <link href="style_co.css" rel="stylesheet">
+		<meta charset="utf-8" />
+        <title>Accueil</title>
+        <link href="../css/style_co.css" rel="stylesheet" />
 
     </head>
     <body>
@@ -50,16 +49,20 @@ if (isset($_POST['connexion']) && $_POST['connexion'] == 'Connexion') {
 
             <section class="login">
                 <div class="titre">Maths Quest</div>
-                <form action="#" method="post">
-                    <div class="bouton">Nom d'utilisateur</div>
+                <form action="accueil.php" method="post">
+                    <div class="bouton">Nom d'utilisateur:
                     <input type="text" required title="Username" placeholder="Username" data-icon="U"> </br>
-                    </br>
-                    <div class="bouton">Mot de passe</div>
-                    <input type="password" required title="Password" placeholder="Password" data-icon="x">
-                    <div class="oubli">
-                        <div class="col"><a href="#" title="Retrouver mot de passe">Forgot Password ?</a></div>
                     </div>
+					</br>
+                    <div class="bouton">Mot de passe:
+                    <input type="password" required title="Password" placeholder="Password" data-icon="x"></div>
+                    <div class="oubli">
                     <a href="#" class="envoyer">Submit</a>
+					</br>
+					</br>
+					    <div class="col"><a href="#" title="Retrouver mot de passe">Forgot Password ?</a></div>
+                    </div>
+                    
                 </form>
             </section>
         </form>
