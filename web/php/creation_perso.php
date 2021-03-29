d15194db62259c2bd6739a7c43c11f10ea6d31b2
NitoGitHub <victortw3x@gmail.com>
2021-03-29 12:11:03 +0200

    crea

--- web/php/creation_perso.php
+++ web/php/creation_perso.php
@@ -18,30 +18,42 @@
     <!-- Titre création du personnage -->
         <h1>Creation personnage</h1>
 
+
+
+                <!-- Script qui permet d'afficher le numéro suivant du choix -->
+                <script>
+                function clic(a,b){
+                    var ligne = document.getElementById("choix"+a);
+                    var n = parseInt(ligne.innerHTML);
+                    n = ((n+b-1)%6)+1;
+                    ligne.innerHTML = n;
+                }               
+                </script>
             
                 <!-- Menu de création -->
                 <!-- Après le choix du sexe et de la classe -->
                 <!-- &nbsp = espace -->
                 <div class="menu_creation_perso column">
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Tete<span id="choix1">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(1,-1);">&#60</button><a>&nbsp Tete<span id="choix1">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">&#62</button>
                     </div>
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Cheveux<span id="choix2">1</span>/6 &nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(2,-1);">&#60</button><a>&nbsp Cheveux<span id="choix2">1</span>/6 &nbsp</a><button onclick="clic(2,+1);">&#62</button>
                     </div>
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Barbe<span id="choix3">1</span>/6 &nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(3,-1);">&#60</button><a>&nbsp Barbe<span id="choix3">1</span>/6 &nbsp&nbsp&nbsp</a><button onclick="clic(3,+1);">&#62</button>
                     </div>
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Haut<span id="choix4">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(4,-1);">&#60</button><a>&nbsp Haut<span id="choix4">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(4,+1);">&#62</button>
                     </div>
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Bas<span id="choix5">1</span>/6 &nbsp&nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(5,-1);">&#60</button><a>&nbsp Bas<span id="choix5">1</span>/6 &nbsp&nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(5,+1);">&#62</button>
                     </div>
                     <div class="row">
-                        <button onclick="clic(1,-1);"><</button><a>&nbsp Pied<span id="choix6">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(1,+1);">></button>
+                        <button onclick="clic(6,-1);">&#60</button><a>&nbsp Pied<span id="choix6">1</span>/6 &nbsp&nbsp&nbsp&nbsp</a><button onclick="clic(6,+1);">&#62</button>
                     </div>
                 </div>
+
 
                 
             <!-- Aperçu du sprite avec ses changements -->
