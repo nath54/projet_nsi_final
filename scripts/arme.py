d68a5d5e646326cf739a7c154c0ca99dead6543d
LDoan1 <dugraallancelot@gmail.com>
2021-04-30 09:57:57 +0200

    Modifs arme

--- scripts/arme.py
+++ scripts/arme.py
@@ -3,8 +3,17 @@ class arme:
         self.server =server 
         self.id_arme = id_arme
         self.nom = ""
+        self.classe = ""
+        self.niveau = 0
         self.style = ""  ## cac ou distance
         self.portee = "" ## portée d'un projectile, surtout pour le style de combat a distance
-        
-        pass
+        self.position = {"x" : 0 , "y" : 0}
+        self.id_region = 1
+        self.load_arme()
 
+    def load_arme(self):
+
+        sql = """SELECT nom, classe, niveau, style, portee FROM arme WHERE id_arme=? """
+
+        res = self.server.db.requete_db(sql, (self.id_arme,))[0]
+        pass
