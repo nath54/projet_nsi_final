9f7c9ca0775a8b23a80376af2ee8be6f6de530c8
AisuKetsueki <72966980+AisuKetsueki@users.noreply.github.com>
2021-05-07 08:41:31 +0200

    Update rempli_monstres.sql

--- sql/rempli_monstres.sql
+++ sql/rempli_monstres.sql
@@ -60,3 +60,21 @@ INSERT INTO monstre SET
     dgt = "{\"forme\":\"random between\", \"values\":[2, 4]}",
     loot = "[]",
     img_base = "blob.mdp";
+
+INSERT INTO monstre SET
+    id_monstre = 7,
+    nom = "fantôme gentil nv.5",
+    niveau = 5,
+    pv = "{\"forme\":\"random between\", \"values\":[4, 7]}",
+    dgt = "{\"forme\":\"random between\", \"values\":[3, 8]}",
+    loot = "[]",
+    img_base = "fantomegentil.mdp";
+
+INSERT INTO monstre SET
+    id_monstre = 8,
+    nom = "fantôme méchant nv.4",
+    niveau = 5,
+    pv = "{\"forme\":\"random between\", \"values\":[5, 8]}",
+    dgt = "{\"forme\":\"random between\", \"values\":[4, 9]}",
+    loot = "[]",
+    img_base = "fantomemechant.mdp";
