433045416eb4bf4db225efe553a2ebd42c4da0d6
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-05-26 11:16:53 +0200

    preparation de le première quete

--- sql/rempli_quetes.sql
+++ sql/rempli_quetes.sql
@@ -1,7 +1,7 @@
 INSERT INTO quetes SET
     id_quete = 1 ,
     nom " votre première quête",
-    description_ "tuer 5 rats" ,
-    condition ,
+    description_ "tuer 5 rats",
+    condition = "{'classe': '','niveau': 1,'quete': []}",
     objectif ,
-    recompense
+    recompense "{'Argent': ,'XP': ,'Objet': }"
