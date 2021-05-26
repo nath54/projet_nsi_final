e2ede6c321df1b81c538639c91132259ff086072
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-05-26 11:02:01 +0200

    rempli quete

---
+++ sql/rempli_quetes.sql
@@ -0,0 +1,9 @@
+INSERT INTO quetes SET
+    id_quete = 1 ,
+    nom " votre première quête",
+    description_ "tuer 5 rats" ,
+    condition ,
+    objectif ,
+    recompense
+
+    lemoinea54460@gmail.com
