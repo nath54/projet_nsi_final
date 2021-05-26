6aa65bea3550106d3c07bfff9fbcf33f49ed109a
ExTer3012 <72519253+ExTer3012@users.noreply.github.com>
2021-05-26 11:26:46 +0200

    oublie

--- sql/rempli_quetes.sql
+++ sql/rempli_quetes.sql
@@ -1,6 +1,6 @@
 INSERT INTO quetes SET
     id_quete = 1 ,
-    nom " votre première quête",
+    nom "votre première quête",
     description_ "tuer 5 rats",
     condition = "{'classe': '','niveau': 1,'quete': []}",
     objectif ,
