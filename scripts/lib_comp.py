c9e4e759a1b9170a7a919f9f8f26a163e9363f55
monkylog <79305220+monkylog@users.noreply.github.com>
2021-05-31 10:49:30 +0200

    manger db

--- scripts/lib_comp.py
+++ scripts/lib_comp.py
@@ -89,7 +89,7 @@ def gere_competences(ws_serv, websocket, data, id_user):
             return
         else:
             p = server.personnages[id_user]
-            p.vie += p.vie_max*0.1
+            p.vie += p.vie_max*0.2
             if p.vie > p.vie_max:
                 p.vie = p.vie_max
             server.send_to_user(p.id_utilisateur, {"action":"vie", "value":p.vie, "max_v": p.vie_max})
